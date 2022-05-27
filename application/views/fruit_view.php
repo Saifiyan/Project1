13788a07a6f805d74c3f98b40dd404bd83bad47d
Saifiyan <saifiyanshaikh6@gmail.com>
2022-05-27 06:10:21 -0700

    update func

--- application/views/fruit_view.php
+++ application/views/fruit_view.php
@@ -78,7 +78,51 @@
         </div>
     </div>
 </form>
+<!-- -->
 
+<!-- Modal -->
+<form action="" id="form">
+    <div class="modal fade" id="Modal_edit" tabindex="-1" role="dialog" aria-labelledby="exampleModelLabel" aria-hidden="true" data-keyboard="false" data-backdrop="static">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Edit Product</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <input type="hidden" name="fruit_id_edit" id="fruit_id_edit" class="form-control">
+                    
+                    <div class="form-group row">
+                        <label class="col-md-2 col-form-label">Fruit Name</label>
+                        <div class="col-md-10">
+                            <input type="text" name="fruit_name_edit" id="fruit_name_edit" class="form-control">
+                        </div>
+                    </div>
+                    <!-- 
+                    working with this input[type="text"]    
+                    <div class="form-group row">
+                        <label class="col-md-2 col-form-label">Fruit id</label>
+                        <div class="col-md-10">
+                            <input type="text" name="fruit_id" id="fruit_id" class="form-control" placeholder="first id">
+                        </div>
+                    </div> -->
+                    <div class="form-group row">
+                        <label class="col-md-2 col-form-label"> Price</label>
+                        <div class="col-md-10">
+                            <input type="number" name="price_edit" id="price_edit" class="form-control" >
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" type="submit" id="btn_update" class="btn btn-primary">Update</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
 
 
     <!-- Optional JavaScript -->
@@ -107,7 +151,7 @@
                             '<td>'+data[i].id+'</td>'+                            
                             '<td>'+data[i].name+'</td>'+                            
                             '<td>'+data[i].price+'</td>'+
-                            '<td><a href="javascript:void(0);" class="btn btn-info btn-sm item-edit" data-fruit_id="'+data[i].id+'" data-fruit_name="'+data[i].name+'" data-price="'+data[i].price+'">EDIT</a>'+' '+'<a href="javascript:void(0);" class="btn btn-danger btn-sm item_delete" data-fruit_id="'+data[i].id+'">DELETE</a>'+'</td>'+'</tr>';
+                            '<td><a href="javascript:void(0);" class="btn btn-info btn-sm item_edit" data-fruit_id="'+data[i].id+'" data-fruit_name="'+data[i].name+'" data-price="'+data[i].price+'">EDIT</a>'+' '+'<a href="javascript:void(0);" class="btn btn-danger btn-sm item_delete" data-fruit_id="'+data[i].id+'">DELETE</a>'+'</td>'+'</tr>';
                         }
 
                         $('#show_data').html(html);
@@ -154,6 +198,18 @@
                     alert("Please Enter Something");
                 }
             });
+        });
+
+        $('#show_data').on('click','.item_edit', function(){
+            var fruit_id = $(this).data('fruit_id');
+            var fruit_name = $(this).data('fruit_name');
+            var price = $(this).data('price');
+
+            $('#Modal_edit').modal('show');
+            $('[name="fruit_id_edit"]').val(fruit_id);
+            $('[name="fruit_name_edit"]').val(fruit_name);
+            $('[name="price_edit"]').val(price);
+
         });
     </script>
   </body>
